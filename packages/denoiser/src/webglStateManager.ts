e51f92b3519d9206ffc5d8f0a26dcf0b632960cf
Dennis Smolek <desopolis@gmail.com>
2024-07-31 21:26:12 +0900

    Add options to webGLStateManager

--- packages/denoiser/src/webglStateManager.ts
+++ packages/denoiser/src/webglStateManager.ts
@@ -1,13 +1,43 @@
+type WebGLState = {
+    program: WebGLProgram,
+    activeTexture: number,
+    arrayBuffer: WebGLBuffer,
+    elementArrayBuffer: WebGLBuffer,
+    framebuffer: WebGLFramebuffer,
+    renderbuffer: WebGLRenderbuffer,
+    texture: WebGLTexture,
+    viewport: number[],
+    blend: boolean,
+    depthTest: boolean,
+    cullFace: boolean,
+    scissorTest: boolean,
+    scissor: number[],
+    blendFunc: number[],
+    blendEquation: number[],
+    colorMask: boolean[],
+    clearColor: number[],
+    pixelStoreParams: {
+        unpackAlignment: number,
+        unpackFlipY: boolean,
+        unpackPremultiplyAlpha: boolean,
+        unpackColorspaceConversion: number
+    }
+}
+
 export class WebGLStateManager {
     private gl: WebGLRenderingContext;
-    private tfState: any = null;
-    private capturedState: any = null;
+    private tfState: WebGLState | null = null;
+    private capturedState: WebGLState | null = null;
+    public ingoreRestore = false;
+    public debugging = false;
+
+
 
     constructor(gl: WebGLRenderingContext) {
         this.gl = gl;
     }
 
-    private captureState(): any {
+    private captureState(): WebGLState {
         const gl = this.gl;
         return {
             program: gl.getParameter(gl.CURRENT_PROGRAM),
@@ -46,12 +76,15 @@ export class WebGLStateManager {
 
     captureCurrentState() {
         this.capturedState = this.captureState();
+        this.logState(this.capturedState, 'Captured Current State NO TF');
     }
 
-    saveState(ignoreRestore = false) {
+    saveState(ignoreRestore = this.ingoreRestore) {
         this.tfState = this.captureState();
+        this.logState(this.tfState, 'Saved TF State');
         if (!ignoreRestore && this.capturedState) {
             this.applyState(this.capturedState);
+            this.logState(this.capturedState, 'Restored Original State');
         }
     }
 
@@ -59,10 +92,12 @@ export class WebGLStateManager {
         if (!this.tfState) return;
 
         this.capturedState = this.captureState();
+        this.logState(this.capturedState, 'Captured Original State');
         this.applyState(this.tfState);
+        this.logState(this.tfState, 'Restored TF State');
     }
 
-    private applyState(state: any) {
+    private applyState(state: WebGLState) {
         const gl = this.gl;
 
         gl.useProgram(state.program);
@@ -90,40 +125,10 @@ export class WebGLStateManager {
         gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, state.pixelStoreParams.unpackPremultiplyAlpha);
         gl.pixelStorei(gl.UNPACK_COLORSPACE_CONVERSION_WEBGL, state.pixelStoreParams.unpackColorspaceConversion);
     }
-}
 
-/* Cody code
-    restoreWebGLState() {
-        if (!this.gl || !this.webglProgram || !this.webglState) return;
-        const gl = this.gl;
-        const state = this.webglState;
-        if (state !== null) {
-            gl.bindVertexArray(state.VAO);
-            gl.activeTexture(state.activeTexture);
-            gl.bindTexture(gl.TEXTURE_2D, state.textureBinding);
-            gl.useProgram(state.program);
-            gl.bindRenderbuffer(gl.RENDERBUFFER, state.renderbufferBinding);
-            gl.bindFramebuffer(gl.FRAMEBUFFER, state.framebufferBinding);
-            //@ts-ignore
-            gl.viewport(...state.viewport);
-
-            if (state.scissorTest) gl.enable(gl.SCISSOR_TEST);
-            else gl.disable(gl.SCISSOR_TEST);
-
-            if (state.stencilTest) gl.enable(gl.STENCIL_TEST);
-            else gl.disable(gl.STENCIL_TEST);
-
-            if (state.depthTest) gl.enable(gl.DEPTH_TEST);
-            else gl.disable(gl.DEPTH_TEST);
-
-            if (state.blend) gl.enable(gl.BLEND);
-            else gl.disable(gl.BLEND);
-
-            if (state.cullFace) gl.enable(gl.CULL_FACE);
-            else gl.disable(gl.CULL_FACE);
-        }
-        // restore the webgl state
-        this.gl.useProgram(this.webglProgram);
-        if (this.debugging) console.log('WebGL State Restored', this.webglProgram, this.gl, this.webglState);
+    logState(state: WebGLState, addedContext = '') {
+        if (!this.debugging) return;
+        console.log(`WebGL State:${addedContext}`);
+        console.table(state);
     }
-        */
+}
